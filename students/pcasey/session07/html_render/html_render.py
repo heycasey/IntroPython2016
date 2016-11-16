a6ac331f69ef1a3099607bec9954be9a123e7bcf
Paul Casey <paul@heycasey.com>
2016-11-15 20:15:13 -0800

    HTML Render added

---
+++ students/pcasey/session07/html_render/html_render.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+"""
+Python class example.
+"""
+
+
+# The start of it all:
+# Fill it all in here.
+
+class Element:
+
+    def __init__(self, content=None):
+        self.content = []
+        if content:
+            self.content.append(content)
+
+    def append(self, new_content):
+        self.content.append(new_content)
+
+    def render(self, file_out):
+        file_out.write("<html>\n")
+        for stuff in self.content:
+            file_out.write(stuff+"\n")
+        file_out.write("</html>\n")
+
+# class Element(object):
+
+#     def __init__(self, content=None):
+#         self.content = content
+#         pass
+#     def append(self, new_content):
+#         pass
+#     def render(self, file_out, ind=""):
+#         file_out.write("just something as a place holder...")
